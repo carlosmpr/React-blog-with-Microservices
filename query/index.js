222cde137c3a2800c7257300358274c414b70d2a
carlosp <carlosmpr0508@gmail.com>
2021-07-19 19:35:35 -0400

    Added event bus and query service

---
+++ query/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors =require('cors')
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+
+})
+
+
+app.post('/events', (req,res) => {
+
+})
+
+app.listen(4002 , ()=>{
+    console.log('Listening on 4002')
+})
